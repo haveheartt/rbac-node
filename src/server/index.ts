a1f41211639996fd6ef7f3ff2e11c32f5690d2e3
gui <guilhermev2huehue@gmail.com>
2024-08-03 16:53:10 -0300

    feat: implementing sign up route with express

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,10 +4,18 @@ import { SignUpUseCase } from '../app/useCases/SignUpUseCase';
 
 const app = express();
 
-app.get('/sign-up', (request, response) => {
+app.use(express.json());
+
+app.post('/sign-up', async (request, response) => {
   const SALT = 10;
   const signUpUseCase = new SignUpUseCase(SALT);
   const signUpController = new SignUpController(signUpUseCase);
+
+  const { statusCode, body } = await signUpController.handle({
+    body: request.body,
+  });
+
+  response.status(statusCode).json(body);
 });
 
 app.listen(6969, () => {
